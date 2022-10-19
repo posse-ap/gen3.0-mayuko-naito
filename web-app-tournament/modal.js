330a1dfbeec6356b8ed962a6e0433af07d423531
“Mayuko <sonyeye0824@gmail.cpm>
2022-10-19 21:26:32 +0900

    モーダル途中まで

---
+++ web-app-tournament/modal.js
@@ -0,0 +1,12 @@
+'use strict';
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+}
